a8abae5c97083b600e311eabcfc4894e2bbd24d3
Mario Reder <mario.reder@pm.me>
2024-05-22 11:18:39 +0200

    deno deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { preprocessMeltUI, sequence } from "@melt-ui/pp";
-import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from "sveltekit-adapter-deno";
 /** @type {import('@sveltejs/kit').Config}*/
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
