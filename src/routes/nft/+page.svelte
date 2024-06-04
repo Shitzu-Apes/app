861a0baa843c20515e53ce2c506e3dcff765e26b
pysrxyz <gm@pysr.xyz>
2024-06-04 13:42:12 +0700

    feat: staking functionality for nft page

--- src/routes/nft/+page.svelte
+++ src/routes/nft/+page.svelte
@@ -1,26 +1,129 @@
 <script lang="ts">
+  import type { HereCall } from "@here-wallet/core";
   import { FixedNumber } from "@tarnadas/fixed-number";
-  import { writable } from "svelte/store";
 
   import NftBanner from "$lib/components/NFTBanner.svelte";
-  // import { wallet } from "$lib/near";
-  import { Nft } from "$lib/near";
+  import { Nft, wallet, type Token } from "$lib/near";
   import { Rewarder } from "$lib/near/rewarder";
 
   let selectedNftTokenId = "";
 
-  // const { accountId$ } = wallet;
-  const accountId$ = writable<string>("lucy_hane.test.near");
+  const { accountId$ } = wallet;
 
-  let primaryNftTokenId = Rewarder.primaryNftOf($accountId$).then(
-    async ([token_id, score]) => {
-      const token = await Nft.nftToken(token_id);
+  let primaryNftTokenId: Promise<{
+    token_id: string;
+    score: FixedNumber;
+    token: Token;
+  } | null> = new Promise((resolve) => resolve(null));
+  function refreshPrimaryNftOf() {
+    if ($accountId$) {
+      primaryNftTokenId = Rewarder.primaryNftOf($accountId$).then(
+        async (somePrimaryNftTokenId) => {
+          if (!somePrimaryNftTokenId) {
+            return null;
+          }
 
-      return { token_id, score, token };
-    },
-  );
+          const [token_id, score] = somePrimaryNftTokenId;
+          const token = await Nft.nftToken(token_id);
 
-  let nfts = Nft.nftTokensForOwner($accountId$);
+          return { token_id, score: new FixedNumber(score, 18), token };
+        },
+      );
+    }
+  }
+
+  let nfts: Promise<Token[] | null> = new Promise((resolve) => resolve(null));
+
+  function refreshNfts() {
+    if ($accountId$) {
+      nfts = Nft.nftTokensForOwner($accountId$);
+    }
+  }
+
+  $: {
+    if ($accountId$) {
+      refreshNfts();
+      refreshPrimaryNftOf();
+    }
+  }
+
+  async function stake() {
+    const transactions: HereCall[] = [];
+    if (await primaryNftTokenId) {
+      transactions.push({
+        receiverId: import.meta.env.VITE_REWARDER_CONTRACT_ID,
+        actions: [
+          {
+            type: "FunctionCall",
+            params: {
+              methodName: "unstake",
+              args: {},
+              gas: 30_000_000_000_000n.toString(),
+              deposit: "0",
+            },
+          },
+        ],
+      });
+    }
+    transactions.push({
+      receiverId: import.meta.env.VITE_NFT_CONTRACT_ID,
+      actions: [
+        {
+          type: "FunctionCall",
+          params: {
+            methodName: "nft_transfer_call",
+            args: {
+              receiver_id: import.meta.env.VITE_REWARDER_CONTRACT_ID,
+              token_id: selectedNftTokenId,
+              msg: "",
+            },
+            gas: 50_000_000_000_000n.toString(),
+            deposit: "1",
+          },
+        },
+      ],
+    });
+    await wallet.signAndSendTransactions(
+      { transactions },
+      {
+        onSuccess: () => {
+          if ($accountId$) {
+            refreshNfts();
+            refreshPrimaryNftOf();
+          }
+        },
+        onFinally: () => {},
+      },
+    );
+  }
+
+  async function unstake() {
+    await wallet.signAndSendTransaction(
+      {
+        receiverId: import.meta.env.VITE_REWARDER_CONTRACT_ID,
+        actions: [
+          {
+            type: "FunctionCall",
+            params: {
+              methodName: "unstake",
+              args: {},
+              gas: 30_000_000_000_000n.toString(),
+              deposit: "0",
+            },
+          },
+        ],
+      },
+      {
+        onSuccess: () => {
+          if ($accountId$) {
+            refreshNfts();
+            refreshPrimaryNftOf();
+          }
+        },
+        onFinally: () => {},
+      },
+    );
+  }
 </script>
 
 <div class="w-full">
@@ -33,31 +136,36 @@
         <div class="i-svg-spinners:pulse-3 size-24" />
       </div>
     {:then token}
-      <div class="flex flex-col justify-center items-start">
-        <img
-          class="size-24 rounded-3xl border-4 border-lime flex-1"
-          src="https://bafybeifqejvrnlzraceyapuzne6d2cl2s5bolosrufpwp3lw22pqfcafo4.ipfs.nftstorage.link/{token.token_id}.png"
-          alt="avatar"
-        />
-      </div>
-      <div
-        class="flex flex-col justify-center items-start text-sm md:text-lg flex-1"
-      >
-        <div class="font-bold text-black leading-tight">
-          #{token.token_id}
+      {#if token}
+        <div class="flex flex-col justify-center items-start">
+          <img
+            class="size-24 rounded-3xl border-4 border-lime flex-1"
+            src="https://bafybeifqejvrnlzraceyapuzne6d2cl2s5bolosrufpwp3lw22pqfcafo4.ipfs.nftstorage.link/{token.token_id}.png"
+            alt="avatar"
+          />
         </div>
-        <div class="font-light text-black leading-tight">
-          {$accountId$}
+        <div
+          class="flex flex-col justify-center items-start text-sm md:text-lg flex-1"
+        >
+          <div class="font-bold text-black leading-tight">
+            #{token.token_id}
+          </div>
+          <div class="font-light text-black leading-tight">
+            {$accountId$}
+          </div>
+          <div class="font-bold text-black">
+            {token.score.format()}
+          </div>
         </div>
-        <div class="font-bold text-black">
-          {new FixedNumber(token.score, 18).format()}
-        </div>
-      </div>
-      <button
-        class="absolute bottom-3 right-3 flex items-center justify-center text-black border-b border-black font-bold text-xs mt-3 self-end ml-auto"
-      >
-        Unstake
-      </button>
+        <button
+          class="absolute bottom-3 right-3 flex items-center justify-center text-black border-b border-black font-bold text-xs mt-3 self-end ml-auto"
+          on:click={unstake}
+        >
+          Unstake
+        </button>
+      {:else}
+        <NftBanner />
+      {/if}
     {:catch}
       <NftBanner />
     {/await}
@@ -66,7 +174,7 @@
   {#await nfts}
     <p>Loading...</p>
   {:then nfts}
-    {#if nfts.length > 0}
+    {#if nfts && nfts.length > 0}
       <h2 class="text-2xl font-bold">Pick Your Primary NFT</h2>
       <ul class="not-prose flex w-full flex-wrap gap-4">
         {#each nfts as nft}
@@ -99,6 +207,7 @@
       </ul>
       <button
         class="w-full bg-gradient-to-r bg-lime text-black font-bold rounded-lg py-3 px-3 mt-3"
+        on:click={stake}
       >
         Set {selectedNftTokenId} as Primary NFT
       </button>
