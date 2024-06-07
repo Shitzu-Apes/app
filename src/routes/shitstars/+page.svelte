a6a43faa97a7ec0e6ceb226066364a9e75a964bf
pysrxyz <gm@pysr.xyz>
2024-06-07 21:34:39 +0700

    refactor: store ranking using derived

--- src/routes/shitstars/+page.svelte
+++ src/routes/shitstars/+page.svelte
@@ -2,85 +2,11 @@
   import { slide } from "svelte/transition";
 
   import { Button, Faq, Donation, Squircle } from "$lib/components";
-  import { Nft } from "$lib/near";
   import { wallet } from "$lib/near";
-  import { Rewarder } from "$lib/near/rewarder";
-  import { primaryNftTokenId, refreshPrimaryNftOf } from "$lib/store";
-  import { FixedNumber } from "$lib/util";
+  import { refreshPrimaryNftOf } from "$lib/store";
+  import { ranking } from "$lib/store/ranking";
 
   const BASE_URL = import.meta.env.VITE_NFT_BASE_URL;
-
-  let ranking: Promise<
-    { token_id: string; account_id: string; score: FixedNumber }[]
-  > = Rewarder.getLeaderboard(10).then(async (ranking) => {
-    const primaryNft = await $primaryNftTokenId;
-
-    const nonStakedNfts: string[] = ranking
-      .flatMap(([, account]) => {
-        const nonStaked = account.filter(([, accountId]) => accountId === null);
-        return nonStaked.map(([tokenId]) => tokenId);
-      })
-      .filter((tokenId) => tokenId !== null)
-      .flat() as string[];
-
-    const nonStakedTokens = await Promise.all(
-      nonStakedNfts.map(async (tokenId) => {
-        const owner = await Nft.nftToken(tokenId);
-        return [tokenId, owner.owner_id];
-      }),
-    ).then((tokens) => {
-      const tokenMap = tokens.reduce(
-        (acc, [tokenId, accountId]) => {
-          acc[tokenId] = accountId;
-          return acc;
-        },
-        {} as Record<string, string>,
-      );
-
-      return tokenMap;
-    });
-
-    // the ranking is already sorted
-    // we just need to flatten the array
-    let isInRanking = false;
-    const result = ranking
-      .flatMap(([score, accounts]) =>
-        accounts.map((account) => {
-          const token_id = account[0];
-          let account_id = account[1];
-
-          if (account_id === null) {
-            if (token_id in nonStakedTokens) {
-              account_id = nonStakedTokens[token_id];
-            } else {
-              account_id = "Anonymous";
-            }
-          }
-
-          if (token_id === primaryNft?.token_id) {
-            account_id = "You";
-            isInRanking = true;
-          }
-
-          return {
-            token_id: account[0],
-            account_id,
-            score: new FixedNumber(score.toString(), 18),
-          };
-        }),
-      )
-      .slice(0, 10);
-
-    if (!isInRanking && primaryNft) {
-      result.push({
-        token_id: primaryNft.token_id,
-        account_id: "You",
-        score: primaryNft.score,
-      });
-    }
-
-    return result;
-  });
 
   const { accountId$ } = wallet;
 
@@ -94,7 +20,7 @@
 <div>
   <div class="w-full flex flex-col -mt-1">
     <div class="not-prose">
-      {#await ranking}
+      {#await $ranking}
         <div
           transition:slide
           class="flex items-center justify-center w-full h-xs"
