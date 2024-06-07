ed03635177a30718b428ff4537f7ec996c9e3c29
pysrxyz <gm@pysr.xyz>
2024-06-07 11:37:21 +0700

    feat: add stake status

--- src/routes/shitstars/[token_id]/+page.svelte
+++ src/routes/shitstars/[token_id]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { format } from "d3-format";
+  import { slide } from "svelte/transition";
 
   import type { PageData } from "./$types";
 
@@ -104,33 +105,68 @@
   });
 </script>
 
-<main class="not-prose w-full flex flex-col">
-  <a href="/shitstars">
+<main class="not-prose w-full flex flex-col items-center justify-center">
+  <a href="/shitstars" class="mr-auto">
     <div class="i-mdi:chevron-left size-8" />
   </a>
   {#await token}
-    <div class="i-svg-spinners:pulse-3 size-40" />
+    <div transition:slide class="i-svg-spinners:pulse-3 size-40" />
   {:then token}
-    <div class="w-full">
-      <div>
-        <Squircle
-          src="{import.meta.env.VITE_NFT_BASE_URL}/{token_id}.png"
-          class="text-red-3 size-40"
-        />
-        {token.owner}
-      </div>
-      <div>
-        {token.score.format()}
+    <div class="w-full" transition:slide>
+      <div class="w-full flex gap-3 my-6">
+        <div class="relative">
+          <Squircle
+            src="{import.meta.env.VITE_NFT_BASE_URL}/{token_id}.png"
+            class="{token.staker ? 'text-lime' : 'text-red'} size-32"
+          />
+
+          <!-- Badge for staker/non-staker bottom-center -->
+          <div class="absolute bottom-0 left-0 right-0 flex justify-center">
+            <div
+              class="{token.staker
+                ? 'bg-lime'
+                : 'bg-red'} text-black text-xs px-1 py-0.5 rounded-full"
+            >
+              {#if token.staker}
+                <div class="flex items-center gap-1">
+                  <div class="i-mdi:check-decagram size-4" />
+                  Staked
+                </div>
+              {:else}
+                Not Staked
+              {/if}
+            </div>
+          </div>
+        </div>
+        <div class="flex flex-col justify-center flex-1">
+          <div>
+            Shitzu Revival #{token_id}
+          </div>
+          <h2 class="text-xl flex items-center gap-1">
+            {token.owner}
+          </h2>
+          <div
+            class="flex items-center gap-1 text-lg bg-lime w-fit rounded-full px-2 text-black mt-1"
+          >
+            <div class="i-mdi:stars" />
+            {token.score.format()}
+          </div>
+        </div>
       </div>
 
-      <ul class="w-full flex flex-wrap gap-3 justify-center">
+      <ul
+        class="w-full flex flex-wrap justify-center bg-black border border-lime rounded-xl overflow-hidden"
+      >
         {#each token.trait.attributes as { trait_type, value }}
           <li
-            class="w-[45%] bg-black flex flex-col py-2 px-2 rounded-xl border border-lime"
+            class="w-full bg-black flex justify-between items-center border-b border-lime last:border-transparent px-2 py-2"
           >
-            <span class="text-gray-3 text-sm">{trait_type}</span>
-            <span class="my-1">{value}</span>
-            <span class="text-sm bg-red/25 text-red w-fit px-2 rounded"
+            <div class="flex flex-col">
+              <span class="text-gray-3 text-sm">{trait_type}</span>
+              <span class="my-1">{value}</span>
+            </div>
+            <span
+              class="text-sm bg-red/25 text-red-4 w-fit px-2 rounded min-w-20 text-center py-1"
               >{format(".2%")(RARITY[trait_type][value])}</span
             >
           </li>
