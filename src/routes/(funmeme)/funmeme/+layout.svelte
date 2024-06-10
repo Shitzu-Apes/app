779b8d222b8b21489d519e2b6bb76ea719d2b417
pysrxyz <gm@pysr.xyz>
2024-06-10 20:49:40 +0700

    feat: create coming soon page

--- src/routes/(funmeme)/funmeme/+layout.svelte
+++ src/routes/(funmeme)/funmeme/+layout.svelte
@@ -10,8 +10,8 @@
   <div
     in:blur={{ duration: 500, delay: 500, easing: cubicIn }}
     out:blur={{ duration: 500, easing: cubicOut }}
-    class="w-full h-screen bg-blue"
+    class="w-full h-screen bg-gradient-to-t bg-gradient-from-cyan bg-gradient-to-blue"
   >
-    <a href="/shitstars"> Back to SHITZU </a>
+    <slot />
   </div>
 {/key}
--- src/routes/(funmeme)/funmeme/+page.svelte
+++ src/routes/(funmeme)/funmeme/+page.svelte
@@ -1,1 +1,16 @@
-Introducing Fun dot meme without layout
+<div
+  class="mx-auto w-fit h-screen flex flex-col justify-center items-center gap-10 overflow-hidden"
+>
+  <h1
+    class="text-6xl md:text-8xl text-center tracking-widest font-semibold text-white"
+  >
+    Coming <br />Soon
+  </h1>
+  <a
+    href="/"
+    class="border-4 border-lime self-stretch text-center py-4 rounded-xl text-lime animate-pulse animate-duration-1000 font-bold flex items-center justify-center"
+  >
+    <div class="i-mdi:chevron-left size-6" />
+    Back
+  </a>
+</div>
