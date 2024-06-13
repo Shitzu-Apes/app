1b7dc121f58ea03b3fc8c56cad161028aa57663b
pysrxyz <gm@pysr.xyz>
2024-06-13 20:31:31 +0700

    style: change style to funmeme

--- src/routes/(funmeme)/funmeme/+page.svelte
+++ src/routes/(funmeme)/funmeme/+page.svelte
@@ -1,7 +1,3 @@
-<script lang="ts">
-  import { BuiltOnNear } from "$lib/assets";
-</script>
-
 <div
   class="mx-auto w-fit h-screen flex flex-col justify-center items-center gap-10 overflow-hidden"
 >
@@ -10,26 +6,13 @@
   >
     Coming <br />Soon
   </h1>
-  <a
-    href="/"
-    class="text-center py-3 rounded-xl text-black animate-duration-1000 font-bold flex items-center justify-center bg-gradient-to-r bg-gradient-from-lime bg-gradient-to-emerald w-40 md:w-60"
-  >
-    <div class="i-mdi:chevron-left size-6" />
-    Back
-  </a>
+  <a href="/" class="text-white text-xl"> [ Back ] </a>
 
   <div class="mt-6 w-full px-2 py-3 text-white">
     <div class="flex justify-center items-center max-w-lg">
       <div class="flex items-center gap-4">
-        <a href="https://near.org/" target="_blank" rel="noopener">
-          <BuiltOnNear style="height: 1.1rem;" />
-        </a>
-
         <a href="https://twitter.com/" target="_blank" rel="noopener">
-          <div class="i-simple-icons:x w-[1.1rem] h-[1.1rem]" />
-        </a>
-        <a href="https://t.me/" target="_blank" rel="noopener">
-          <div class="i-simple-icons:telegram w-[1.1rem] h-[1.1rem]" />
+          [ Twitter ]
         </a>
       </div>
     </div>
