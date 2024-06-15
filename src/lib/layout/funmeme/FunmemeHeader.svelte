d002c86d479e6df549699e1e1549e1e60fdec3b1
pysrxyz <gm@pysr.xyz>
2024-06-15 12:48:37 +0700

    feat: add header component

---
+++ src/lib/layout/funmeme/FunmemeHeader.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import SHITZU_FACE from "$lib/assets/logo/shitzu_face.svg";
+</script>
+
+<nav class="py-2 px-2 w-full flex justify-between">
+  <div class="flex gap-4 items-center">
+    <a href="/board">
+      <img src={SHITZU_FACE} class="size-8 bg-shitzu-4" alt="Shitzu face" />
+    </a>
+    <ul class="text-sm">
+      <div class="flex gap-2">
+        <li class="mr-2">
+          <a href="https://twitter.com"> [twitter]</a>
+        </li>
+        <li>
+          <a href="https://github.com"> [support]</a>
+        </li>
+      </div>
+      <div class="flex gap-2">
+        <li>
+          <a href="https://discord.com"> [telegram]</a>
+        </li>
+        <li>
+          <a href="https://discord.com"> [how it works]</a>
+        </li>
+      </div>
+    </ul>
+  </div>
+
+  <button class="text-sm"> [connect wallet] </button>
+</nav>
---
+++ src/routes/(funmeme)/board/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import "@unocss/reset/tailwind.css";
+  import "virtual:uno.css";
+  import FunmemeHeader from "$lib/layout/funmeme/FunmemeHeader.svelte";
+</script>
+
+<div class="text-white min-h-screen">
+  <FunmemeHeader />
+  <main>
+    <slot></slot>
+  </main>
+  <footer></footer>
+</div>
