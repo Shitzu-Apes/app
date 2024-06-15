d60dfe107891f8f20be3253c0f244f7129e36dcd
pysrxyz <gm@pysr.xyz>
2024-06-15 16:41:58 +0700

    style: add key for transition

--- src/routes/(funmeme)/board/+layout.svelte
+++ src/routes/(funmeme)/board/+layout.svelte
@@ -4,10 +4,12 @@
   import FunmemeHeader from "$lib/layout/funmeme/FunmemeHeader.svelte";
 </script>
 
-<div class="text-white min-h-screen">
-  <FunmemeHeader />
-  <main>
-    <slot></slot>
-  </main>
-  <footer></footer>
-</div>
+{#key "funmemeboard"}
+  <div class="text-white min-h-screen">
+    <FunmemeHeader />
+    <main>
+      <slot></slot>
+    </main>
+    <footer></footer>
+  </div>
+{/key}
