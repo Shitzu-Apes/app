ad9061491e78657bce11315354d82521e29408a2
pysrxyz <gm@pysr.xyz>
2024-06-25 14:39:02 +0700

    refactor: shared common layout

--- src/routes/(funmeme)/board/+layout.svelte
+++ src/routes/(funmeme)/board/+layout.svelte
@@ -1,17 +1,13 @@
 <script lang="ts">
-  import "@unocss/reset/tailwind.css";
-  import "virtual:uno.css";
   import { BottomSheet } from "$lib/layout/BottomSheet";
   import FunmemeHeader from "$lib/layout/funmeme/FunmemeHeader.svelte";
 </script>
 
-{#key "funmemeboard"}
-  <BottomSheet variant="funmeme" />
-  <div class="text-white min-h-screen">
-    <FunmemeHeader />
-    <main>
-      <slot></slot>
-    </main>
-    <footer></footer>
-  </div>
-{/key}
+<BottomSheet variant="funmeme" />
+<div class="text-white min-h-screen">
+  <FunmemeHeader />
+  <main>
+    <slot></slot>
+  </main>
+  <footer></footer>
+</div>
