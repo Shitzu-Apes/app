491a2a5e7f56aa27a34dae4170a52f3445c93dfe
pysrxyz <gm@pysr.xyz>
2024-06-27 10:37:07 +0700

    chore: use submodule for cloning charting library

--- copy_charting_library_files.sh
+++ copy_charting_library_files.sh
@@ -4,22 +4,14 @@ remove_if_directory_exists() {
 	if [ -d "$1" ]; then rm -Rf "$1"; fi
 }
 
-BRANCH="master";
-
-REPOSITORY="${CHARTING_LIBRARY_REPOSITORY}"
-
-LATEST_HASH=$(git ls-remote $REPOSITORY $BRANCH | grep -Eo '^[[:alnum:]]+')
-
-remove_if_directory_exists "$LATEST_HASH"
-
-git clone -q --depth 1 -b "$BRANCH" $REPOSITORY "$LATEST_HASH"
+submodule="./charting_library"
 
 remove_if_directory_exists "static/charting_library"
 remove_if_directory_exists "src/lib/datafeeds"
 remove_if_directory_exists "src/lib/charting_library"
 
-cp -r "$LATEST_HASH/charting_library" static
-cp -r "$LATEST_HASH/datafeeds" src/lib
-cp -r "$LATEST_HASH/charting_library" src/lib
+cp -r "$submodule/charting_library" static
+cp -r "$submodule/datafeeds" src/lib
+cp -r "$submodule/charting_library" src/lib
 
-remove_if_directory_exists "$LATEST_HASH"
+# remove_if_directory_exists "$submodule"
