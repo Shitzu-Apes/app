ab1e5f9fc1b58bf2fcc4a4725d2d88d53f124f81
pysrxyz <gm@pysr.xyz>
2024-07-01 12:10:54 +0700

    style: layout and tabs for profile page

---
+++ src/routes/(funmeme)/profile/[...accountId]/+page.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  import { createTabs, melt } from "@melt-ui/svelte";
+
+  import { page } from "$app/stores";
+  import SHITZU_POCKET from "$lib/assets/shitzu_pocket.svg";
+
+  const { accountId } = $page.params;
+
+  const tabs = [
+    { id: "staked", label: "coins held" },
+    { id: "created", label: "coin created" },
+    { id: "unstaked", label: "food waste" },
+  ];
+
+  const {
+    elements: { root, list, content, trigger },
+    states: { value },
+  } = createTabs({
+    defaultValue: tabs[0].id,
+  });
+</script>
+
+<section class="w-full flex flex-col items-center justify-center">
+  <div class="my-6">
+    <div class="flex">
+      <img
+        src={SHITZU_POCKET}
+        alt="shitzu pocket"
+        class="size-16 mr-1 text-shitzu-4"
+      />
+      <div>
+        <p class="text-lg">{accountId}</p>
+        <a
+          href="https://pikespeak.ai/wallet-explorer/{accountId}/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="text-sm text-shitzu-4">[view on explorer]</a
+        >
+      </div>
+    </div>
+  </div>
+
+  <div use:melt={$root}>
+    <div use:melt={$list} class="flex gap-1">
+      {#each tabs as tab}
+        <button
+          use:melt={$trigger(tab.id)}
+          class="{tab.id !== $value
+            ? 'text-shitzu-4 bg-transparent'
+            : 'text-dark bg-shitzu-4'} font-400 px-2 rounded"
+        >
+          {tab.label}
+        </button>
+      {/each}
+    </div>
+  </div>
+
+  <section use:melt={$content(tabs[0].id)}>tabs 0</section>
+  <section use:melt={$content(tabs[1].id)}>tabs 1</section>
+  <section use:melt={$content(tabs[2].id)}>tabs 2</section>
+</section>
