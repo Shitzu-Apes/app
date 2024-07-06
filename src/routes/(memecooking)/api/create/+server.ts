168835e885aad62c87bc3adea1a9a9b3fe96b5a8
pysrxyz <gm@pysr.xyz>
2024-07-06 13:32:29 +0700

    refactor: pinata logic into Pinata class

--- src/routes/(memecooking)/api/create/+server.ts
+++ src/routes/(memecooking)/api/create/+server.ts
@@ -1,39 +1,14 @@
+import Pinata from "$lib/server/pinata.js";
+
 export async function POST(e) {
   const form = await e.request.formData();
-
   const imageFile = form.get("imageFile") as File;
-
-  const pinataForm = new FormData();
-  pinataForm.append("file", imageFile);
-  form.append("pinataOptions", '{\n  "cidVersion": 1\n}');
-  const res = await fetch("https://api.pinata.cloud/pinning/pinFileToIPFS", {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${import.meta.env.VITE_PINATA_JWT}`,
-    },
-    body: pinataForm,
-  });
-
-  const json = await res.json();
-  const { IpfsHash: imageCID } = json;
-
   const referenceContent = form.get("reference") as string;
 
+  const { IpfsHash: imageCID } = await Pinata.pinFileToIPFS(imageFile);
+
   const reference = { ...JSON.parse(referenceContent), image: imageCID };
-
-  const uploadReference = await fetch(
-    "https://api.pinata.cloud/pinning/pinJSONToIPFS",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${import.meta.env.VITE_PINATA_JWT}`,
-      },
-      body: JSON.stringify(reference),
-    },
-  );
-
-  const { IpfsHash: referenceCID } = await uploadReference.json();
+  const { IpfsHash: referenceCID } = await Pinata.pinJSONToIPFS(reference);
 
   return new Response(
     JSON.stringify({
