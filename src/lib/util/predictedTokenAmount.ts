5d68c7f1605ff17072248c7f2d01e1f88ad668f7
Mario Reder <mario.reder@pm.me>
2024-08-09 07:06:26 +0200

    fix division by zero

--- src/lib/util/predictedTokenAmount.ts
+++ src/lib/util/predictedTokenAmount.ts
@@ -3,13 +3,16 @@ import { FixedNumber } from "./FixedNumber";
 export function predictedTokenAmount(data: {
   amount: string;
   total_deposit: string;
-  total_supply?: string;
+  total_supply?: string | null;
   decimals?: number;
 }): number {
   const decimals = data.decimals ?? 18;
   console.log(data);
   const amount = BigInt(data.amount);
   const totalDeposit = BigInt(data.total_deposit);
+  if (totalDeposit === 0n) {
+    return 0;
+  }
   const totalSupply = BigInt(
     data.total_supply ?? "1000000000000000000000000000",
   );
