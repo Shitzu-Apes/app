edf8f7800d1efdae6d01cac4ef7403ffab8f5c4f
pysrxyz <gm@pysr.xyz>
2024-08-17 00:24:06 +0700

    add description

--- src/lib/components/memecooking/Board/TokenDetail.svelte
+++ src/lib/components/memecooking/Board/TokenDetail.svelte
@@ -7,24 +7,21 @@
   import { FixedNumber } from "$lib/util";
 
   export let memebid: Meme;
+  let showDescription = false;
 </script>
 
 {#if memebid}
   <div class="flex flex-col w-full h-full items-center">
-    <h2
-      class="flex gap-4 text-2xl my-4 justify-between items-center w-full px-2"
-    >
-      <div>
+    <h2 class="flex text-2xl my-4 justify-between items-center w-full px-2">
+      <div class="flex flex-col">
         {#if memebid.end_timestamp_ms}
           <Countdown to={memebid.end_timestamp_ms} class="text-shitzu-4" />
         {/if}
         <div
           class="flex items-center bg-amber text-white px-2 text-2xl rounded mb-4"
         >
-          <Near className="size-6 -ml-1" />{new FixedNumber(
-            memebid.total_deposit,
-            24,
-          ).format()}
+          <Near className="size-6 -ml-1" />
+          {new FixedNumber(memebid.total_deposit, 24).format()}
         </div>
       </div>
       <div class="flex flex-col items-end">
@@ -34,15 +31,41 @@
             ${memebid.symbol}
           </span>
         </h4>
-        <div class="flex items-center text-xs">
+        <div class="flex items-center justify-end text-xs">
           created by
-          <Chef
-            account={memebid.owner}
-            class="text-sm overflow-hidden text-ellipsis"
-          />
+          <div class="w-1/2">
+            <Chef
+              account={memebid.owner}
+              class="text-sm overflow-hidden text-ellipsis"
+            />
+          </div>
         </div>
       </div>
     </h2>
+    <div class="w-full">
+      <div class="px-4 py-2">
+        <p class="text-sm text-gray-200">
+          {#if memebid.description && memebid.description.length > 100}
+            {#if showDescription}
+              {memebid.description}
+            {:else}
+              {memebid.description.slice(0, 100)}...
+
+              <button
+                on:click={() => {
+                  showDescription = true;
+                }}
+                class="text-shitzu-4 underline"
+              >
+                Read more
+              </button>
+            {/if}
+          {:else}
+            {memebid.description}
+          {/if}
+        </p>
+      </div>
+    </div>
     <div class="w-full h-auto">
       <img
         src="{import.meta.env.VITE_IPFS_GATEWAY}/{memebid.image}"
