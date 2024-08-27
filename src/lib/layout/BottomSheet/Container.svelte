8f7f517e0548b9761531958c3af838081a121694
pysrxyz <gm@pysr.xyz>
2024-08-27 16:50:54 +0700

    add backdrop

--- src/lib/layout/BottomSheet/Container.svelte
+++ src/lib/layout/BottomSheet/Container.svelte
@@ -3,7 +3,6 @@
 
   const open = writable(false);
   const component = writable<{
-    // @esline need to support all Svelte components
     component: typeof SvelteComponent<any>;
     props: Record<string, any>;
   } | null>(null);
@@ -25,29 +24,42 @@
 <script lang="ts">
   import { SvelteComponent } from "svelte";
   import type { colorVariant } from "$lib/models/variant";
+  import { onMount } from "svelte";
 
   export let variant: colorVariant = "lime";
+
+  let mounted = false;
+
+  onMount(() => {
+    mounted = true;
+  });
 </script>
 
-<div
-  class="fixed bg-black bottom-0 left-0 right-0 w-full max-w-[min(30rem,100%)] mx-auto h-[90svh] z-40 transform {$open
-    ? 'translate-y-0'
-    : 'translate-y-full'} duration-500 rounded-t-xl border-3 border-b-0 {variant ===
-  'lime'
-    ? 'border-lime'
-    : 'border-shitzu-4'}"
->
-  <div class="h-full">
-    <button
-      class="absolute top-3 right-3 {variant === 'lime'
-        ? 'bg-lime'
-        : 'bg-shitzu-4'} rounded-full flex justify-center items-center px-1 py-1"
-      on:click={close}
-    >
-      <div class=" block i-mdi:close size-6 ml-auto bg-black" />
-    </button>
-    {#if $component}
-      <svelte:component this={$component.component} {...$component.props} />
-    {/if}
+{#if $open && mounted}
+  <div class="fixed inset-0 bg-black/80 z-30" on:click={close}></div>
+{/if}
+
+{#if mounted}
+  <div
+    class="fixed bg-black bottom-0 left-0 right-0 w-full max-w-[min(30rem,100%)] mx-auto h-[90svh] z-40 transform {$open
+      ? 'translate-y-0'
+      : 'translate-y-full'} duration-500 rounded-t-xl border-3 border-b-0 {variant ===
+    'lime'
+      ? 'border-lime'
+      : 'border-shitzu-4'}"
+  >
+    <div class="h-full">
+      <button
+        class="absolute top-3 right-3 {variant === 'lime'
+          ? 'bg-lime'
+          : 'bg-shitzu-4'} rounded-full flex justify-center items-center px-1 py-1"
+        on:click={close}
+      >
+        <div class=" block i-mdi:close size-6 ml-auto bg-black" />
+      </button>
+      {#if $component}
+        <svelte:component this={$component.component} {...$component.props} />
+      {/if}
+    </div>
   </div>
-</div>
+{/if}
