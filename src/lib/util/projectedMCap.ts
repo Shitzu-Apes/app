9db6c3861b8c10f9baf1df46fcde55a310a99114
pysrxyz <gm@pysr.xyz>
2024-09-10 18:09:46 +0700

    mcap of live on ref

--- src/lib/util/projectedMCap.ts
+++ src/lib/util/projectedMCap.ts
@@ -3,14 +3,49 @@ import { derived, writable, type Readable, type Writable } from "svelte/store";
 import { FixedNumber } from "./FixedNumber";
 
 import type { Meme } from "$lib/api/client";
+import { Ref } from "$lib/near";
 
 export function projectedMCap(meme: Meme): Readable<FixedNumber> {
-  return derived(nearPrice, (price) => {
-    getNearPrice();
-    return new FixedNumber(BigInt(meme.total_deposit) * 2n, 24).mul(
-      new FixedNumber(price, 24),
+  if (meme.pool_id) {
+    const tokenPrice = writable<bigint>(0n);
+    getTokenPrice(meme.pool_id, meme.decimals).then((price) =>
+      tokenPrice.set(price),
     );
-  });
+
+    return derived([nearPrice, tokenPrice], ([$nearPrice, $tokenPrice]) => {
+      getNearPrice();
+      return new FixedNumber(
+        $tokenPrice * BigInt(meme.total_supply!),
+        meme.decimals + 24,
+      ).mul(new FixedNumber($nearPrice, 24));
+    });
+  } else {
+    return derived(nearPrice, (price) => {
+      getNearPrice();
+      return new FixedNumber(BigInt(meme.total_deposit) * 2n, 24).mul(
+        new FixedNumber(price, 24),
+      );
+    });
+  }
+}
+
+async function getTokenPrice(
+  poolId: number,
+  decimals: number,
+): Promise<bigint> {
+  const pool = await Ref.getPool(poolId);
+
+  const denomIdx = pool.token_account_ids.indexOf(
+    import.meta.env.VITE_WRAP_NEAR_CONTRACT_ID,
+  );
+  const tokenIdx = denomIdx === 0 ? 1 : 0;
+
+  const denomAmount = pool.amounts[denomIdx];
+  const tokenAmount = pool.amounts[tokenIdx];
+
+  return new FixedNumber(BigInt(denomAmount), 24)
+    .div(new FixedNumber(BigInt(tokenAmount), decimals))
+    .toBigInt();
 }
 
 export const nearPrice = writable<bigint>(0n);
