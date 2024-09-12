beecf8c8bafaf8f5106a7daf2479b82b37c6e9d9
pysrxyz <gm@pysr.xyz>
2024-09-12 17:44:56 +0700

    don't prerender shitzu app

--- src/routes/(shitzu)/+layout.server.ts
+++ src/routes/(shitzu)/+layout.server.ts
@@ -1,2 +1,13 @@
+import { redirect } from "@sveltejs/kit";
+
+import type { LayoutServerLoad } from "./$types";
+
+const load: LayoutServerLoad = async (props) => {
+  if (props.url.origin.includes("meme.cooking")) {
+    redirect(303, "/board");
+  }
+};
+
 export const ssr = false;
-export const prerender = true;
+export const prerender = false;
+export { load };
