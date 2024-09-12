8caea64b9f4d1610780df4945dfe21bd83114aa7
pysrxyz <gm@pysr.xyz>
2024-09-12 16:37:30 +0700

    logs varaible

--- src/routes/(shitzu)/+page.server.ts
+++ src/routes/(shitzu)/+page.server.ts
@@ -3,7 +3,13 @@ import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
 const load: PageServerLoad = async (props) => {
-  if (props.url.origin.includes("meme.cooking")) {
+  const shitzu = {
+    urlOrigin: props.url.origin,
+    memecookingHostname: import.meta.env.VITE_MEMECOOKING_HOSTNAME,
+    urlHostname: props.url.hostname,
+  };
+  console.log("shitzuapp load", shitzu);
+  if (props.url.hostname === import.meta.env.VITE_MEMECOOKING_HOSTNAME) {
     redirect(303, "/appetizer");
   }
 };
