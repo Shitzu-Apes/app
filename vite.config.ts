e561f5e36b7c070d83878619a3ff9f92eb08e712
pysrxyz <gm@pysr.xyz>
2024-10-18 16:01:07 +0700

    make app pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import extractorSvelte from "@unocss/extractor-svelte";
+import { SvelteKitPWA } from "@vite-pwa/sveltekit";
 import path from "node:path";
 import UnoCSS from "unocss/vite";
 import { defineConfig } from "vite";
@@ -12,6 +13,44 @@ export default defineConfig({
     }),
     sveltekit(),
     nodePolyfills(),
+    SvelteKitPWA({
+      strategies: "generateSW",
+      injectRegister: "inline",
+      registerType: "autoUpdate",
+      includeAssets: ["assets/favicon.ico", "assets/apple-touch-icon.png"],
+      manifest: {
+        name: "Shitzu App",
+        short_name: "Shitzu",
+        description: "Shitzu App - Your gateway to the Shitzu ecosystem",
+        theme_color: "#222222",
+        icons: [
+          {
+            src: "assets/pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "assets/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "assets/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "assets/maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 3 * 1024 * 1024, // 3 MiB
+      },
+    }),
   ],
   resolve: {
     alias: {
